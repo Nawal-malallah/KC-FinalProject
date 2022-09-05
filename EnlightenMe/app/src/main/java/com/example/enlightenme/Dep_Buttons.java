5213a062b6d99ca6d58c2e385e1893988121ffe9
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2022-09-05 14:13:39 +0300

    checkpoint

--- EnlightenMe/app/src/main/java/com/example/enlightenme/Dep_Buttons.java
+++ EnlightenMe/app/src/main/java/com/example/enlightenme/Dep_Buttons.java
@@ -2,7 +2,10 @@ package com.example.enlightenme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dep_Buttons extends AppCompatActivity {
 
@@ -10,5 +13,74 @@ public class Dep_Buttons extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dep_buttons);
+
+        Button depOV = findViewById(R.id.depOV);
+        Button depsym = findViewById(R.id.depSym);
+        Button depKidTeen = findViewById(R.id.depKidTeen);
+        Button depAdult = findViewById(R.id.depAdult);
+        Button depDoc = findViewById(R.id.depDoc);
+        Button depEmergancy = findViewById(R.id.depEmergency);
+
+
+        depOV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent depOV = new Intent(Dep_Buttons.this , Dep_Buttons.class);
+
+                startActivity(depOV);
+            }
+        });
+
+        depsym.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent depsym = new Intent(Dep_Buttons.this , Dep_symptoms.class);
+
+                startActivity(depsym);
+            }
+        });
+
+        depKidTeen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent depKidTeen = new Intent(Dep_Buttons.this , Dep_sympKidTeen.class);
+
+                startActivity(depKidTeen);
+            }
+        });
+
+        depAdult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent depAdult = new Intent(Dep_Buttons.this , Dep_sympAdults.class);
+
+                startActivity(depAdult);
+            }
+        });
+
+        depDoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent depDoc = new Intent(Dep_Buttons.this , Dep_seeDoctor.class);
+
+                startActivity(depDoc);
+            }
+        });
+
+        depEmergancy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent depEmergancy = new Intent(Dep_Buttons.this , Dep_emergencyHelp.class);
+
+                startActivity(depEmergancy);
+            }
+        });
+
     }
 }
--- EnlightenMe/app/src/main/java/com/example/enlightenme/MainActivity.java
+++ EnlightenMe/app/src/main/java/com/example/enlightenme/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.enlightenme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //the cover thingy with yellow head
+
+        Button agree = findViewById(R.id.agree);
+
+        agree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent agree = new Intent(MainActivity.this , BlueButtons.class);
+
+                startActivity(agree);
+            }
+        });
     }
 }
