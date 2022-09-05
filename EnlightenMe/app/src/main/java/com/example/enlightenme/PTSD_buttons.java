2c71396c09ada8c78d7c2b0d869eee95f8c1c40e
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2022-09-05 16:52:11 +0300

    checkpoint

--- EnlightenMe/app/src/main/java/com/example/enlightenme/PTSD_buttons.java
+++ EnlightenMe/app/src/main/java/com/example/enlightenme/PTSD_buttons.java
@@ -2,7 +2,10 @@ package com.example.enlightenme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PTSD_buttons extends AppCompatActivity {
 
@@ -10,5 +13,40 @@ public class PTSD_buttons extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ptsd_buttons);
+
+        Button PTSDov = findViewById(R.id.PTSDov);
+        Button PTSDsym = findViewById(R.id.PTSDsym);
+        Button PTSDdoc = findViewById(R.id.PTSDdoc);
+
+        PTSDov.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent PTSDov = new Intent(PTSD_buttons.this , PTSD_overview.class);
+
+                startActivity(PTSDov);
+            }
+        });
+
+        PTSDsym.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent PTSDsym = new Intent(PTSD_buttons.this , PTSD_symptoms.class);
+
+                startActivity(PTSDsym);
+            }
+        });
+
+        PTSDdoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent PTSDdoc = new Intent(PTSD_buttons.this , PTSD_seeDoctor.class);
+
+                startActivity(PTSDdoc);
+            }
+        });
+
     }
 }
