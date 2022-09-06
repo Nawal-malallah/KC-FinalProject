a13cca47c4669fd86ffaea2a5b86a7b70f0c94f0
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2022-09-06 23:52:35 +0300

    checkpoint

--- EnlightenMe/app/src/main/java/com/example/enlightenme/GAD_symptoms.java
+++ EnlightenMe/app/src/main/java/com/example/enlightenme/GAD_symptoms.java
@@ -2,7 +2,10 @@ package com.example.enlightenme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GAD_symptoms extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class GAD_symptoms extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gad_symptoms);
+
+        Button GADsymNxt1 = findViewById(R.id.GADsymNxt1);
+
+        GADsymNxt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent GADsymNxt1 = new Intent(GAD_symptoms.this , GAD_symptoms_2.class);
+
+                startActivity(GADsymNxt1);
+            }
+        });
     }
 }
--- EnlightenMe/app/src/main/java/com/example/enlightenme/GAD_symptoms_2.java
+++ EnlightenMe/app/src/main/java/com/example/enlightenme/GAD_symptoms_2.java
@@ -2,7 +2,10 @@ package com.example.enlightenme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GAD_symptoms_2 extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class GAD_symptoms_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gad_symptoms2);
+
+        Button GADsymNxt2 = findViewById(R.id.GADsymNxt2);
+
+        GADsymNxt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent GADsymNxt2 = new Intent(GAD_symptoms_2.this , GAD_symptoms_3.class);
+
+                startActivity(GADsymNxt2);
+            }
+        });
     }
 }
