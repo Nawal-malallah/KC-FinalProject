b5a57c5718984147ed35ac349b16b58a28c4d725
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2022-09-07 11:22:08 +0300

    checkpoint

--- EnlightenMe/app/src/main/java/com/example/enlightenme/PTSD_symptoms.java
+++ EnlightenMe/app/src/main/java/com/example/enlightenme/PTSD_symptoms.java
@@ -2,7 +2,10 @@ package com.example.enlightenme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PTSD_symptoms extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class PTSD_symptoms extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ptsd_symptoms);
+
+        Button PTSDsymNxt = findViewById(R.id.PTSDsymNxt);
+
+        PTSDsymNxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent PTSDsymNxt = new Intent(PTSD_symptoms.this , PTSD_symptoms_2.class);
+
+                startActivity(PTSDsymNxt);
+            }
+        });
     }
 }
