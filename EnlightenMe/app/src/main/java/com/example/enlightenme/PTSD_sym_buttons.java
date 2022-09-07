85d16ef50d6fc0d710642cdc9ca6aef8cbd5919d
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2022-09-07 14:29:56 +0300

    checkpoint

---
+++ EnlightenMe/app/src/main/java/com/example/enlightenme/PTSD_sym_buttons.java
@@ -0,0 +1,14 @@
+package com.example.enlightenme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PTSD_sym_buttons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ptsd_sym_buttons);
+    }
+}
