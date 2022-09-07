f67c22c97cf7562b4c60c2b83cd5a543accc8eb4
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2022-09-07 14:38:34 +0300

    check point

--- EnlightenMe/app/src/main/java/com/example/enlightenme/PTSD_sym_buttons.java
+++ EnlightenMe/app/src/main/java/com/example/enlightenme/PTSD_sym_buttons.java
@@ -2,7 +2,10 @@ package com.example.enlightenme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PTSD_sym_buttons extends AppCompatActivity {
 
@@ -10,5 +13,61 @@ public class PTSD_sym_buttons extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ptsd_sym_buttons);
+
+        Button symAviod = findViewById(R.id.symAviod);
+        Button symChanPER = findViewById(R.id.symChanPER);
+        Button symChanTM = findViewById(R.id.symChanTM);
+        Button symIntensity = findViewById(R.id.symIntensity);
+        Button symIntrusive = findViewById(R.id.symIntrusive);
+
+        symAviod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent sym_aviod = new Intent(PTSD_sym_buttons.this , PTSD_symp_avoidance.class);
+
+                startActivity(sym_aviod);
+            }
+        });
+
+        symChanPER.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent sym_PER = new Intent(PTSD_sym_buttons.this , PTSD_symp_chanReaction.class);
+
+                startActivity(sym_PER);
+            }
+        });
+
+        symChanTM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent sym_TM = new Intent(PTSD_sym_buttons.this , PTSD_symp_chanThinkMood.class);
+
+                startActivity(sym_TM);
+            }
+        });
+
+        symIntensity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent sym_TENSE = new Intent(PTSD_sym_buttons.this , PTSD_symp_intensity.class);
+
+                startActivity(sym_TENSE);
+            }
+        });
+
+        symIntrusive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent sym_Intrusive = new Intent(PTSD_sym_buttons.this , PTSD_symp_intrusive.class);
+
+                startActivity(sym_Intrusive);
+            }
+        });
     }
 }
